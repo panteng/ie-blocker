7fac9882dffeacbe12cf529a72e16b18f906ce91
Chris Pan <chrispan.pt@gmail.com>
2015-10-09 12:39:26 +0800

    v.0.1.0 init

---
+++ browser-blocker/browser-blocker.js
@@ -0,0 +1,29 @@
+/*!
+ * browser blocker v0.1.0
+ * (c) 2015 Chris Pan
+ * Released under the MIT License.
+ */
+window.onload = function () {
+    var bbContainer,
+        browserIcons,
+        scriptPath,
+        browserName;
+
+    bbContainer = document.getElementById('bb-container');
+    
+    // the path to the image cannot be relative in a filter. Have to use javascript to get the absolute path.
+    browserIcons = bbContainer.getElementsByTagName('i');
+    for (var i = 0; i < document.scripts.length; i++) {
+        if (scriptPath = document.scripts[i].src.match(/(.*)browser-blocker\.js/)[1]) {
+            break;
+        }
+    }
+    for (var i = 0; i < browserIcons.length; i++) {
+        if (browserName = browserIcons[i].className.match(/bb-ua-(\w+)/)[1]) {
+            browserIcons[i].style['filter'] = 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' +
+            scriptPath + 'img/' + browserName + '.png")';
+        }
+    }
+
+    bbContainer.style.display = 'block';
+};
