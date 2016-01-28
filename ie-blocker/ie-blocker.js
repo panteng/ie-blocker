ac9ef43264d7f15e7a56a5c839afc2e18ecb160b
Chris Pan <chrispan.pt@gmail.com>
2016-01-28 16:25:13 +0800

    Add img-path option and change version to 1.2.0.

--- ie-blocker/ie-blocker.js
+++ ie-blocker/ie-blocker.js
@@ -9,23 +9,26 @@ window.onload = function () {
     var scriptPath;
     var browserName;
     var imgPath;
-    var options;
 
     ibContainer = document.getElementById('ib-container');
 
     // the path to the image cannot be relative in a filter. Have to use javascript to get the absolute path.
     browserIcons = ibContainer.getElementsByTagName('i');
+    
     for (var i = 0; i < document.scripts.length; i++) {
 
         if (match = document.scripts[i].src.match(/(.*)ie-blocker\.js/)) {
             scriptPath = match[1];
+            imgPath = scriptPath + (document.scripts[i].getAttribute('img-path') || 'img/');
+            console.log(imgPath);
             break;
         }
     }
+    
     for (var i = 0; i < browserIcons.length; i++) {
         if (browserName = browserIcons[i].className.match(/ib-ua-(\w+)/)[1]) {
             browserIcons[i].style['filter'] = 'progid:DXImageTransform.Microsoft.AlphaImageLoader(src="' +
-            scriptPath + 'img/' + browserName + '.png")';
+            imgPath + browserName + '.png")';
         }
     }
 
